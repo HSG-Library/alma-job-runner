88d2201f1470dc22a1b0b9c8b6d721d4a9d30941
Jonas Furrer <jonas.furrer@gmail.com>
2023-01-18 13:36:09 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+## Mirconaut Project Dev-Notes
+Generated project with the following Micronaut CLI command:
+
+    mn create-app --build=maven --jdk=17 --lang=java --test=junit --features=logback ch.unisg.library.systemlibrarian.alma-schedule-manual-jobs
---
+++ src/test/java/ch/unisg/library/systemlibrarian/AlmaManualJobRunnerTest.java
@@ -0,0 +1,21 @@
+package ch.unisg.library.systemlibrarian;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.inject.Inject;
+
+@MicronautTest
+class AlmaManualJobRunnerTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
